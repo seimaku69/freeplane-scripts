6cfa2c41a04766098ffac4d5cff5cde13bfa285f
Markus Seilnacht <seimaku@proton.me>
2025-07-24 14:08:00 +0200

    Changed Output, Refactoring

--- ListDueTasks.groovy
+++ ListDueTasks.groovy
@@ -1,20 +1,24 @@
 // @ExecutionModes({ON_SELECTED_NODE})
 
 // author : Markus Seilnacht
-// date : 2025-07-02
+// date : 2025-07-24
 // (c) licensed under GPL-3.0 or later
 
 
 /*
-    This script finds all tasks (atribute tType == 'TASK') which are due today or former.
-    if they have a tStatus-attribute == 'DONE' they are excluded..
-
+    This script finds all tasks (atribute tType == tTask) which are due today or former.
+    if they have an attribute tStatus == tDone they are not listed.
     It creates a child with direct link to each found task.
 
-    Attention : The value for 'DUE DATE' have to follow ISO-8601 ('yyyy-MM-dd')
+    Attention : The value for 'DUE DATE' (tDueDate) have to follow ISO-8601 ('yyyy-MM-dd')
 */
 
 /*
+    #todo 01 : Behandlung von parse-Errors für anderes Datumsformat
+
+    #todo 02 : Ausgabe analog zu 'ListAllTasks'
+
+    #todo 03 : Formel im Attribut des 'taskNode'
 */
 
 import java.time.LocalDate
@@ -22,7 +26,6 @@ import java.time.LocalDate
 final lf = System.lineSeparator()
 
 def sb = new StringBuilder()
-def int counter = 0
 
 // defines which attribute is signed as task
 final String tType = "TYPE"
@@ -35,60 +38,50 @@ final tDueDate = "DUE DATE"
 
 LocalDate now = LocalDate.now()
 
+// create new child node from actual selected
 taskNode = node.createChild()
-taskNode.format = "markdownPatternFormat"
-taskNode.setNoteContentType("markdown")
-//tasks.setNoteContentType("auto")
-//taskNode.setFree(true)
-
-//taskNode.style.setFloating(true)
+taskNode.setFormat("markdownPatternFormat")
+taskNode.setNoteContentType("auto")
+taskNode.setFree(true)
 taskNode.style.setBackgroundColorCode("#FFFF00")
 taskNode.style.setTextColorCode("#333333")
 
 // set timestamp
-taskNode.text = "**Tasklist (due today) " + '**'
+taskNode.text = "**Tasklist (Due Tasks) " + "**"
 taskNode.attributes = ["Date" : format(new Date(), "yyyy-MM-dd  HH:mm:ss")]
-//taskNode.attributes = ["Date" : now.toString()]
 taskNode.attributes.optimizeWidths()
 
-sb << '### Tasklist  ' << lf
-sb << '*(' << tDueDate << ' <= ' << now.toString() << ' && ' << tStatus << ' != ' << tDone << ')*  ' << lf
-sb << '  ' << lf
+sb << "Tasklist (Due Tasks)" << lf
+sb << lf
+sb << "The childs of this node are all successors of [$node.text] with attributes :" << lf
+sb << lf
+sb << "$tType  == $tTask && $tStatus != $tDone && $tDueDate <= " << now.toString() << lf
+sb << lf
+sb << "They provide a direct link to each task.." << lf
 
-//node.findAll().each {
+taskNode.attributes = ["Date" : format(new Date(), "yyyy-MM-dd  HH:mm:ss")]
+
 for (child in node.findAll()) {
     dueVal = child.attributes.getTransformed().getFirst(tDueDate)
     isTask = (child.attributes.getFirst(tType) == tTask) ? true : false
-    // check actual status
-    // child.attributes.getFirst(tStatus) returns NULL if attribute does not exist 
-    // => NULL != tDone - task withaout a tStatus are recognized !
-    // getTransformed().getFirst(..) - value after evaluation of formulas
     status = child.attributes.getTransformed().getFirst(tStatus)
-
-    if (!dueVal || !isTask) continue
+    if (!dueVal || !isTask || status == tDone) continue
     dueDate = LocalDate.parse(dueVal)
 
     if (dueDate.compareTo(now) <= 0) {
-        if (status != tDone) {
-            counter = counter + 1
-            // write note-text
-            sb << '  ' << lf << '*Task(' << counter.toString() << ') :* **' << child.text << '**  ' << lf
-            // Create child with link for each open task in its color
-            newChild = taskNode.createChild()
-            newChild.style.setBackgroundColorCode(child.style.getBackgroundColorCode())
-            newChild.style.setTextColorCode(child.style.getTextColorCode())
-            newChild.format = "markdownPatternFormat"
-            newChild.text = '**' + child.text + '**'
-            newChild.link.text = '#' + child.id
-        }
+        // Create child with link for each open task in original color
+        newChild = taskNode.createChild()
+        newChild.style.setBackgroundColorCode(child.style.getBackgroundColorCode())
+        newChild.style.setTextColorCode(child.style.getTextColorCode())
+        newChild.format = "markdownPatternFormat"
+        newChild.setNoteContentType("auto")
+        newChild.text = "**$child.text**"
+        newChild.link.text = "#$child.id"
     }
 }
 
-sb << '  ' << lf
-sb << '  ' << lf << '..found ' << counter.toString() << ' Task(s).  ' << lf
-taskNode.attributes.add("Task(s)", counter.toString())
-
+// add attribute with formula
+taskNode.attributes.add("Task(s)", "=children.size()")
 taskNode.note = sb.toString()
 taskNode.setFolded(true)
-
 c.select(taskNode)
