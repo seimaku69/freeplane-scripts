38df2ca70b5ceb673023fec2fce6b59c311f7048
Markus Seilnacht <seimaku@proton.me>
2025-08-13 19:21:53 +0200

    Added functionality for optional use of scriptConfig.

--- ListDueTasks.groovy
+++ ListDueTasks.groovy
@@ -1,13 +1,14 @@
 // @ExecutionModes({ON_SELECTED_NODE})
 
 // author : Markus Seilnacht
-// date : 2025-07-24
+// date : 2025-08-13
 // (c) licensed under GPL-3.0 or later
 
 
 /*
     This script finds all tasks (atribute tType == tTask) which are due today or former.
     if they have an attribute tStatus == tDone they are not listed.
+    ..or use optional scriptConfig.json.
     It creates a child with direct link to each found task.
 
     Attention : The value for 'DUE DATE' (tDueDate) have to follow ISO-8601 ('yyyy-MM-dd')
@@ -16,27 +17,45 @@
 /*
     #todo 01 : Behandlung von parse-Errors für anderes Datumsformat
 
-    #todo 02 : Ausgabe analog zu 'ListAllTasks'
-
     #todo 03 : Formel im Attribut des 'taskNode'
 */
 
 import java.time.LocalDate
+import groovy.json.JsonSlurper
 
 final lf = System.lineSeparator()
 
 def sb = new StringBuilder()
 
 // defines which attribute is signed as task
-final String tType = "TYPE"
-final String tTask = "TASK"
+String tType = "TYPE"
+String tTask = "TASK"
 // defines which attribute is checked for status and Done-Value
-final String tStatus = "STATUS"
-final String tDone = "DONE"
+String tStatus = "STATUS"
+String tDone = "DONE"
 // checks this attribute against today
-final tDueDate = "DUE DATE"
+String tDueDate = "DUE DATE"
 
 LocalDate now = LocalDate.now()
+
+
+// Path for config file - overwrites setting above if exists
+final String userDir = c.getUserDirectory().getPath()
+final File configFile = new File(userDir + File.separator + "scriptConfig.json")
+
+// Search for external configuration parameters - overwrite internal ones !
+if (configFile.exists()) {
+    JsonSlurper jsSlurper = new JsonSlurper()
+	Map jsMap = jsSlurper.parse(configFile)
+    tasks = jsMap['tasks.params']
+    if (tasks) {
+        if (tasks['type.identifier']) tType = tasks['type.identifier']
+        if (tasks['type.task']) tTask = tasks['type.task']
+        if (tasks['status.identifier']) tStatus = tasks['status.identifier']
+        if (tasks['status.done']) tDone = tasks['status.done']
+        if (tasks['duedate.identifier']) tDueDate = tasks['duedate.identifier']
+    }
+}
 
 // create new child node from actual selected
 taskNode = node.createChild()
